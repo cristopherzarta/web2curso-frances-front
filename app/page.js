2fbae831bd0af986f4bfe5ff840af53773e2a64c
CristopherZartaR CODE <zartacristopher@gmail.com>
2024-07-09 16:29:02 -0500

    pele

--- app/page.js
+++ app/page.js
@@ -1,7 +1,23 @@
 export default function Home() {
   return (
-    <div>
+    <div className=" bg-blue-400 text-3xl ">
       <h1>Mi wed de CURSOS</h1>
+      <p>
+        Nació el 23 de octubre de 1940 en la localidad brasileña de Tres
+        Corazones, en el estado Minas Gerais. Pelé es considerado por muchos
+        como el mejor futbolista del siglo. Comenzó a jugar fútbol como se
+        comienza a jugar fútbol en Brasil: en las calles. Jugaba con sus
+        compañeros de escuela o con sus vecinos. Sin embargo, su carrera como
+        futbolista profesional comenzó a los 13 años cuando se integró a los
+        juveniles del Club Atlético de Baurú. Su primer entrenador fue Valdemar
+        de Brito, un futbolista que jugó con la selección nacional brasileña en
+        la Copa Mundial de 1934. Fue De Brito quien llevó a Pelé al equipo
+        Santos de San Pablo; y con el Santos, vino la consagración y los
+        mundiales. En la final del mundial del 58 y con tan sólo 17 años, Pelé
+        fue toda una revelación: dos goles espectaculares del joven futbolista
+        dieron a Brasil un triunfo demoledor de 5 - 2 contra Suecia, el equipo
+        anfitrión.
+      </p>
     </div>
   );
 }
