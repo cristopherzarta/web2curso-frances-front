0e10048e50d51e0396447c69b751ce33de61a682
CristopherZartaR CODE <zartacristopher@gmail.com>
2024-07-24 16:55:44 -0500

    cambios

--- app/profile/page.js
+++ app/profile/page.js
@@ -5,19 +5,17 @@ import { useRouter } from "next/navigation";
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../layout";
 import Header from "@/components/Header";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCirclePlay, faLock } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay, faLock } from "@fortawesome/free-solid-svg-icons";
 
-
-export default function Profile () {
-  const { state, dispatch} = useContext(AuthContext);
- 
+const profile = () => {
+  const { state, dispatch } = useContext(AuthContext);
 
   const searchParams = useSearchParams();
   const token = searchParams.get("login_info");
 
   const router = useRouter();
-  
+
   //console.log(router)
 
   // console.log(searchParams);
@@ -26,27 +24,24 @@ export default function Profile () {
   useEffect(() => {
     //console.log({ jwt: searchParams.get("jwt") });
 
-
     if (searchParams.get("login_info")) {
-
       const login_info = JSON.parse(searchParams.get("login_info"));
-    
-    dispatch (
-      {
-        type: 'LOGIN',
-         payload: login_info 
-        }
-      )
+
+      dispatch({
+        type: "LOGIN",
+        payload: login_info,
+      });
       router.replace("/profile");
     }
-  }, [searchParams.get?.login_info])
-  return  ( <>
-  <Header />
-  <div>PROFILE</div>
-  <FontAwesomeIcon icon={faCirclePlay} className='cviolet mh5'/>
-  <FontAwesomeIcon icon={faLock} />
-</>
-  )
+  }, [searchParams.get?.login_info]);
+  return (
+    <>
+      <Header />
+      <div>PROFILE</div>
+      <FontAwesomeIcon icon={faCirclePlay} className="cviolet mh5" />
+      <FontAwesomeIcon icon={faLock} />
+    </>
+  );
 };
 
-
+export default profile;
