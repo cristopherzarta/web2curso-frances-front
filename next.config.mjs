2226f09508e01b72f5f4c17219ff2f2d6aaf6935
CristopherZartaR CODE <zartacristopher@gmail.com>
2024-07-29 18:57:46 -0500

    headers 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,29 +1,34 @@
 /** @type {import('next').NextConfig} */
+//const ContentSecPolicy = `default-src 'self' ka-f.fontawesome.com localhost:4000 web-curso-frances.azurewebsites.net webcursosfrances.blob.core.windows.net; script-src 'self' 'unsafe-eval' kit.fontawesome.com; style-src 'self' 'unsafe-inline' *.fontawesome.com fonts.googleapis.com; font-src 'self' fonts.googleapis.com ka-f.fontawesome.com;  kit.fontawesome.com; img-src 'self' data:`;
+
 const nextConfig = {
-    images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "webcursosfrances.blob.core.windows.net",
-        pathname: "**",
-      },
-    ],
+  reacStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ["webcursosfrances.blob.core.windows.net"],
   },
 
-  async headers() {
+  /*async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
-            value: `style-src 'self' *.fontawesome.com fonts.googleapis.com `,
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
           },
-         
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecPolicy,
+          },
         ],
       },
-    ]
-  },
+    ];
+  },*/
 };
 
 export default nextConfig;
