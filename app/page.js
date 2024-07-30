972f433c089edc629b2c66b0cd82226b438c6f92
CristopherZartaR CODE <zartacristopher@gmail.com>
2024-07-30 07:59:55 -0500

    image

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ export default function Home() {
         <span>- Mahatma Gandhi</span>
       </div>
 
-      <h2 className="mt20 tdu">CURSOS</h2>
+      <h2 className="mt20 tdu" style={{ margintop: "5rem"}}>CURSOS</h2>
 
       <div className="mt10">
         {courses.map((c) => (
--- components/CourseCard.js
+++ components/CourseCard.js
@@ -7,26 +7,28 @@ import { useContext } from "react";
 import Swal from "sweetalert2";
 
 const CourseCard = ({ course }) => {
-
-  const { thumbnail, name, description, _id } = course
+  const { thumbnail, name, description, _id } = course;
 
   const router = useRouter();
 
   const handleClick = () => {
-   
-    
     router.push(`/courses/study/${_id}`);
     localStorage.setItem(_id, JSON.stringify(course));
   };
 
   return (
     <>
-      <div className="df jcsb cursorp card-container" onClick={handleClick}>
+      <div
+        className="df jcsb cursorp card-container posr"
+        onClick={handleClick}
+      >
         <div
           style={{
             boxShadow: "0 2px 5px rgba(0,0,0,0.2)",
             marginRight: "1rem",
-            width: `${0.82 * 150}px`,
+            width: `${0.82 * 10}rem`,
+            height: `10rem`,
+            overflow: "hidden",
           }}
         >
           <Image
@@ -35,12 +37,12 @@ const CourseCard = ({ course }) => {
             alt="Picture of the author"
             width={0.82 * 150}
             height={150}
-           
+            layout="responsive"
           />
         </div>
 
-        <div className="df fdc card-body mt5">
-          <h3 className="cblack">{name}</h3>
+        <div className="df fdc card-body ">
+          <h3 className="cwhite">{name}</h3>
           <h1 className="cgrey m0">{description}</h1>
         </div>
       </div>
@@ -48,24 +50,27 @@ const CourseCard = ({ course }) => {
       <style jsx>{`
         .card-container {
           flex-direction: column;
-          background-color: white;          
-          width: ${0.82 * 150 + "px"};
-          padding: 0.75rem;
+          background-color: var(--white);
+          width: ${0.82 * 15 + "rem"};
           border-radius: 0.5rem;
-          max-width: 50rem;
+          overflow: hidden;
+          box-shadow: 0 0 10px rgba(255, 255, 255, 0.4);
           transition: transform 0.3s ease;
+          margin: 0 auto;
+          
         }
 
         .card-body {
-            width: 100%;
-          }
-       
+          width: 100%;
+          padding: 0.75rem;
+          background-color: var(--black)
+          
+        }
+
         .card-container:hover {
           transform: scale(1.03);
           box-shadow: 0 0 10px rgba(255, 255, 255, 0.4);
         }
-
-       
       `}</style>
     </>
   );
