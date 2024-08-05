79c17804cda2c373835faf2a50a4851a89a671c0
CristopherZartaR CODE <zartacristopher@gmail.com>
2024-08-05 11:55:22 -0500

    cambios client-paypal

--- components/CourseVideo.js
+++ components/CourseVideo.js
@@ -13,21 +13,21 @@ const CourseVideo = ({
   coursePrice,
   setCourse,
   }) => {
-  const couldWatch = (isAuthenticated && hasBoughtTheCourse) || isFree;
+  const couldWatch = (isAuthenticated && hasBoughtTheCourse) || isFree
 
-  const router = useRouter();
+  const router = useRouter()
 
-  console.log({ howManySales });
+  //console.log({ howManySales });
 
-  const today = Date.now();
+  //const today = Date.now();
   let price =
     howManySales < 25
-      ? (coursePrice * 0.3).toFixed(0)
-      : (coursePrice * 0.7).toFixed(0);
-  const offerExpirationDate = new Date(2024, 9, 31, 17, 12);
+      ? (coursePrice * 0.4).toFixed(0)
+      : (coursePrice * 0.6).toFixed(0)
+  const offerExpirationDate = new Date(2024, 9, 31, 17, 12)
 
   if (isPast(offerExpirationDate)) {
-    price = coursePrice;
+    price = coursePrice
   }
   //console.log({
   //isPast: isPast(new Date(2024, 6, 31, 17, 12)),
@@ -39,7 +39,7 @@ const CourseVideo = ({
       style={{
         overflow: "hidden",
         height: "28rem",
-        boxShadow: "0 3px 5px rgba(255,255,255,0.2)",
+        boxShadow: "0 2px 10px rgba(255,255,255,0.2)",
       }}
     >
       {couldWatch && (
@@ -58,17 +58,14 @@ const CourseVideo = ({
         </p>
       )}
       {!couldWatch && isAuthenticated && (
-        <div className=" ">
+        <div className="df aic fdc">
           <p> Para visualizar este video primero deberias adquirir el curso </p>
-          <PayPalButtons
-            price={price}
-            courseId={courseId}
-            setCourse={setCourse}
-          />
+
+          <PayPalButtons price={2} courseId={courseId} setCourse={setCourse} />
         </div>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default CourseVideo;
+export default CourseVideo
