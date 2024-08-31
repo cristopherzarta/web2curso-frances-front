d457456b9a721dd778a97d766d29a5d68b5b5ffd
CristopherZartaR CODE <zartacristopher@gmail.com>
2024-08-31 12:40:13 -0500

    content security policy3

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 //const ContentSecPolicy = `default-src 'self' ka-f.fontawesome.com localhost:4000 web-curso-frances.azurewebsites.net webcursosfrances.blob.core.windows.net; script-src 'self' 'unsafe-eval' kit.fontawesome.com; style-src 'self' 'unsafe-inline' *.fontawesome.com fonts.googleapis.com; font-src 'self' fonts.googleapis.com ka-f.fontawesome.com;  kit.fontawesome.com; img-src 'self' data:`;
 
-const ContentSecPolicy = `default-src 'self' www.sandbox.paypal.com ka-f.fontawesome.com localhost:4000 web-curso-frances.azurewebsites.net webcursosfrances.blob.core.windows.net; script-src 'self' www.paypal.com kit.fontawesome.com 'unsafe-inline' 'unsafe-eval' ; style-src 'self' 'unsafe-inline' *.fontawesome.com ; font-src 'self' ka-f.fontawesome.com; img-src 'self' www.paypalobjects.com data:`;
+const ContentSecPolicy = `default-src 'self' www.paypal.com www.sandbox.paypal.com ka-f.fontawesome.com localhost:4000 web-curso-frances.azurewebsites.net webcursosfrances.blob.core.windows.net; script-src 'self' www.paypal.com kit.fontawesome.com 'unsafe-inline' 'unsafe-eval' ; style-src 'self' 'unsafe-inline' *.fontawesome.com ; font-src 'self' ka-f.fontawesome.com; img-src 'self' t.paypal.com www.paypalobjects.com data:`;
 
 const nextConfig = {
   images: {
